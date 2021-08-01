148ecfc2cd4e1796a994b39948d64667d1b9a7b2
Snowlionking <winandylex_@hotmail.de>
2021-08-01 13:38:44 +0200

    fetching and adding to database added displaying name multiverseid and image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Cards from './components/cards'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: '',
+            multiverseId: -1,
+            cards: []
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleFetchSubmit = this.handleFetchSubmit.bind(this);
+        this.handleAddSubmit = this.handleAddSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ searchTerm: event.target.value });
+        this.setState({ multiverseId: event.target.value });
+    }
+
+    handleFetchSubmit(event) {
+        alert('A name was submitted: ' + this.state.searchTerm);
+        event.preventDefault();
+
+        fetch('http://localhost:8080/api/card/' + this.state.searchTerm)
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ cards: data })
+            })
+            .catch(console.log)
+    }
+
+    handleAddSubmit(event) {
+        alert('A card was added to the database: ' + this.state.multiverseId);
+        event.preventDefault();
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+        };
+
+        fetch('http://localhost:8080/api/card/' + this.state.multiverseId, requestOptions)
+            .catch(console.log)
+    }
+
+    render() {
+        return (
+            <><form onSubmit={this.handleFetchSubmit}>
+                <label>
+                    SearchTerm:
+                    <input type="text" value={this.state.searchTerm} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Search" />
+            </form>
+                <Cards cards={this.state.cards} />
+                <form onSubmit={this.handleAddSubmit}>
+                    <label>
+                        MultiverseId:
+                        <input type="text" value={this.state.multiverseId} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Add" />
+                </form>
+            </>
+        );
+    }
 }
 
 export default App;
